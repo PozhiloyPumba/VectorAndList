a62511e191ec01e5f0d665092d1b74d798832854
pozhiloyPumba <ivanov.in@phystech.edu>
2022-02-07 17:36:32 +0300

    insertion sort test

--- test_sort.c
+++ test_sort.c
@@ -3,17 +3,52 @@
 
 #include "vector.h"
 
-void sort (struct vector *vec);
+void insertion_sort (struct vector *vec);
+
+void swap (struct vector *vec, size_t first, size_t second);
 
 int main ()
 {
-    int size;
-    int err = scanf ("%d", &size);
-    assert(err);
+    size_t size;
+    int err = scanf ("%ld", &size);
+    assert (err);
 
-    struct vector *vec = vector_construct (size);
-    vector_init (vec);
+    struct vector *vec = vector_create ();
+
+    Type value;
+    for (size_t i = 0; i < size; ++i) {
+        err = scanf ("%d", &value);
+        assert (err);
+        vector_push_back (vec, value);
+    }
+
+    insertion_sort (vec);
+
+    vector_dump (vec);
+    printf ("\n");
+
     vector_destruct (vec);
 
     return 0;
+}
+
+void insertion_sort (struct vector *vec)
+{
+    size_t size = vector_get_size (vec);
+
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t j = i; j > 0; --j) {
+            if (vector_get_i_th (vec, j - 1) <= vector_get_i_th (vec, j))
+                break;
+
+            swap (vec, j - 1, j);
+        }
+    }
+}
+
+void swap (struct vector *vec, size_t first, size_t second)
+{
+    Type tmp = vector_get_i_th (vec, first);
+    vector_set_i_th (vec, vector_get_i_th (vec, second), first);
+    vector_set_i_th (vec, tmp, second);
 }
