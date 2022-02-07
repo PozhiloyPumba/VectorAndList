505d4aeb0cc6b9d1ce9a46ff36f418d8e3d8f485
pozhiloyPumba <ivanov.in@phystech.edu>
2022-02-07 17:35:17 +0300

    vector complete

--- vector.c
+++ vector.c
@@ -1,46 +1,77 @@
 #include "vector.h"
 
 struct vector{
-    vecType *arr_;
-    int size_;
+    Type *arr_;
+    size_t capacity_;
+    size_t size_;
 };
 
 //-----------------------------------------------------------------------------------------------------
 
-struct vector *vector_construct (int size)
+struct vector *vector_create ()
 {
     struct vector * vec = (struct vector *) malloc (sizeof (struct vector) * 1);
 
-    vec->arr_ = (vecType *) malloc (sizeof (vecType) * size);
-    vec->size_ = size;
+    vec->size_ = 0;
+    vec->capacity_ = 1;
+    vec->arr_ = (Type *) malloc (sizeof (Type) * vec->capacity_);;
 
     return vec;
 }
 
 //-----------------------------------------------------------------------------------------------------
 
-void vector_init (struct vector *vec)
+void extention_vec (struct vector *vec)
 {
-    int err;
-    for (int i = 0; i < vec->size_; ++i){
-        err = scanf ("%d", (vec->arr_) + i);
-        assert (err);
-    }
+    Type *tmp = (Type *) malloc (sizeof (Type) * vec->capacity_ * 2);
+
+    for (size_t i = 0; i < vec->size_; ++i)
+        tmp[i] = vec->arr_[i];
+
+    free (vec->arr_);
+    vec->arr_ = tmp;
+    vec->capacity_ = vec->capacity_ * 2;
 }
 
 //-----------------------------------------------------------------------------------------------------
 
-void vector_print (const struct vector *vec)
+int vector_push_back (struct vector *vec, Type value)
 {
-    for (int i = 0; i < vec->size_; ++i)
+    if (vec->size_ >= vec->capacity_)
+        extention_vec (vec);
+
+    vec->arr_[vec->size_++] = value;
+
+    return 0;
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+void vector_dump (const struct vector *vec)
+{
+    for (size_t i = 0; i < vec->size_; ++i)
         printf ("%d ", vec->arr_[i]);
 }
 
 //-----------------------------------------------------------------------------------------------------
 
-vecType vector_get_i_th (const struct vector *vec, int i)
+Type vector_get_i_th (const struct vector *vec, size_t i)
 {
     return vec->arr_[i];
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+void vector_set_i_th (const struct vector *vec, Type value, size_t i)
+{
+    vec->arr_[i] = value;
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+size_t vector_get_size (const struct vector *vec)
+{
+    return vec->size_;
 }
 
 //-----------------------------------------------------------------------------------------------------
--- vector.h
+++ vector.h
@@ -5,15 +5,16 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef int vecType;
+typedef int Type;
 
 struct vector;
 
-struct vector *vector_construct (int size);
-void vector_init (struct vector *vec);
-void vector_print (const struct vector *vec);
-vecType vector_get_i_th (const struct vector *vec, int i);
-vecType vector_remove_i_th (const struct vector *vec, int i);
+struct vector *vector_create ();
+int vector_push_back (struct vector *vec, Type value);
+void vector_dump (const struct vector *vec);
+Type vector_get_i_th (const struct vector *vec, size_t i);
+void vector_set_i_th (const struct vector *vec, Type value, size_t i);
+size_t vector_get_size (const struct vector *vec);
 void vector_destruct (struct vector *vec);
 
 #endif
