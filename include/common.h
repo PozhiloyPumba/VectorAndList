09eb92856857cc1412af574cc8356330bcd7eee4
pozhiloyPumba <ivanov.in@phystech.edu>
2022-02-16 13:44:46 +0300

    base

--- include/common.h
+++ include/common.h
@@ -4,10 +4,16 @@
 
 typedef int Type;
 
-struct base {
-    int (*push_back) (void *, Type);
-    Type (*get_i_th) (void *, size_t, int *);
-    int (*set_i_th) (const void *, Type, size_t);
-    size_t (*get_size) (const void *);
-    void (*destroy) (void *);
-};
+struct container;
+
+typedef struct base {
+    int (*push_back) (struct container *, Type);
+    Type (*get_i_th) (struct container *, size_t, int *);
+    int (*set_i_th) (const struct container *, Type, size_t);
+    size_t (*get_size) (const struct container *);
+    void (*destroy) (struct container *);
+} base;
+
+typedef struct container {
+    base *m;
+} container;
